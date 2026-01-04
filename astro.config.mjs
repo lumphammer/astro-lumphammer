ff7d5ff0bcf1b1eb8aa41d4e2d98eb56d8879626
Neil de Carteret <git@lumphammer.net>
2026-01-04 13:24:22 +0000

    set target on external links in markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,17 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import rehypeExternalLinks from "rehype-external-links";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        { target: "_blank", rel: ["noopener", "nofollow", "noreferrer"] },
+      ],
+    ],
+  },
 });
