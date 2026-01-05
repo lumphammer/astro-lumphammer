d48f93f4410348978cc0bdebb2150dc4ba3bdd16
Neil de Carteret <git@lumphammer.net>
2026-01-05 12:13:33 +0000

    Revert "remove random crap"
    This reverts commit a1942a95a509b17d48f16aa5ece7b770c31f4a38.

--- src/components/NavigationLink.tsx
+++ src/components/NavigationLink.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useContext } from "react";
+import { OnNavigateContext } from "./OnNavigateContext";
 import type { PropsWithChildren } from "react";
 import { useNavigationContext } from "./NavigationContext";
 
