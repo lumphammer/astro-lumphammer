98a2acd9233f7d141d4e2b99b8d0c5668fe84956
Neil de Carteret <git@lumphammer.net>
2026-03-17 09:11:43 +0000

    be explicit about static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  output: "static",
 
   markdown: {
     rehypePlugins: [
